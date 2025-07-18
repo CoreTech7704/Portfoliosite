8b7a36822379c920880febae172669a8f5dcdccf
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2025-07-18 11:02:18 +0530

    Contact me section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 const Contact = () => {
   return (
-    <section className="max-w-6xl mx-auto px-4 md:px-20 mt-24" id="contact"> 
+    <div id="contact">
+      <section className="max-w-6xl mx-auto px-4 md:px-20 mt-24"> 
       <div className="text-center mb-16">
         <h2 className="text-5xl font-bold mb-4">Contact me</h2>
         <div className="h-1 w-45 bg-gradient-to-r from-purple-500 to-purple-700 rounded-full mt-2 mx-auto" />
@@ -10,7 +11,8 @@ const Contact = () => {
         <a href="https://github.com/CoreTech7704" target="_blank">GitHub</a>
         <a href="https://linkedin.com/in/sarvam-patel" target="_blank">LinkedIn</a>
       </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
