31e64d19901c5f069bb92fd876b566af56927fc8
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2025-07-18 18:48:56 +0530

    Hero Section upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="bg-zinc-950 text-white min-h-screen">
+    <div className="relative bg-zinc-950 text-white min-h-screen overflow-hidden">
       <Navbar />
       <Home />
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
+import { ChevronDown, ArrowDownCircle, MousePointerClick } from "lucide-react";
+
 const Hero = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex flex-col justify-center items-center px-4 text-center bg-zinc-950 text-white"
+      className="relative min-h-screen flex flex-col justify-center items-center px-4 text-center bg-zinc-950 text-white"
     >
       <h1 className="text-4xl sm:text-5xl md:text-5xl font-extrabold mb-6 leading-tight">
         Hey, I'm Sarvam
@@ -23,16 +25,21 @@ const Hero = () => {
       </a>
 
       {/* Scroll Down indicator */}
-      <div className="absolute bottom-6 animate-bounce"> 
+      <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-10 animate-bounce">
         <a href="#about" aria-label="Scroll down">
           <svg
-            className="w-6 h-6 text-purple-500 hover:text-purple-500 transition"
+            xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
             viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8 text-purple-500 hover:text-purple-700 transition"
           >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
+            />
           </svg>
         </a>
       </div>
